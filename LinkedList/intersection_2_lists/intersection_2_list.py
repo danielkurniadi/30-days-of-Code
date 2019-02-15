eaf558540d3793972da77683c0266a59a482bb75
Daniel Kurniadi <daniel.thekurniadi@gmail.com>
2019-02-15 23:08:51 +0800

    solve Intersection of Two Linked Lists in python3  using double iteration and id hashing

---
+++ LinkedList/intersection_2_lists/intersection_2_list.py
@@ -0,0 +1,39 @@
+
+class ListNode:
+    """Definition of linked list node"""
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    """ 
+        Problem statement: 
+        Assuming no cycle in linked list 1 and 2, find the intersection in linked list 1 and 2. If there isn't, 
+    
+    """
+    def getIntersectionNode(self, A, B):
+        """
+        @param A : head node of linked list 1
+        @param B : head node of linked list 2
+        @return: the intersection node in the linked list
+        """
+        self.hashSet = {}
+        # traverse linked list 1
+        current = A
+        while not current:
+            if not id(current) in hashSet:
+                self.hashSet[id(current)] = 1
+            else: 
+                return current
+        # traverse linked list 2
+        current = B
+        while not current:
+            if not id(current) in hashSet:
+                self.hashSet[id(current)] = 1
+            else :
+                return current
+        # no intersection
+        return None
+        
+
+        
