1309b09b07696636d02eaebd6a8da1d55ed969ca
Daniel Kurniadi <daniel.thekurniadi@gmail.com>
2019-02-27 23:40:32 +0800

    solver Prettify JSON problem in python3

---
+++ Strings/prettyJSON/prettyJSON.py
@@ -0,0 +1,43 @@
+class Solution:
+    # @param A : string
+    # @return a list of strings
+    def prettyJSON(self, json):
+        json = "%r"%json
+        length = len(json)
+        tabs, is_outer_dict = 0, True
+        is_newline = True
+        pretty_json = ""
+        i = 0
+        while i < length:
+            ch = json[i]
+            if (ch == '{') or  (ch == '['):
+                if not is_outer_dict:
+                    pretty_json += '\n'
+                is_outer_dict = False
+                pretty_json += '\t'*tabs + ch+ '\n'
+                tabs += 1
+                i+=1
+                is_newline = True
+                continue
+            if (ch == '}') or (ch == ']'):
+                tabs -= 1
+                pretty_json += '\n' + '\t'*tabs + ch + '\n'
+                i+=1
+                is_newline = True
+                continue
+            if is_newline:
+                pretty_json += '\t'*tabs
+                is_newline = False
+            pretty_json += ch
+            if ch == ',':
+                pretty_json += '\n'
+                is_newline = True
+            i +=1
+        print(pretty_json)
+
+if __name__ == '__main__':
+    solver = Solution()
+    solver.prettyJSON(r'["foo", {"bar":["baz",null,1.0,2]}]')
+    solver.prettyJSON(r'{A:"B",C:{D:"E",F:{G:"H",I:"J"}}}')
+    solver.prettyJSON('{"id":100,"firstName":"Jack","lastName":"Jones","age":12}')
+            
