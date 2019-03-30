8115262e5c50b544b86b5610adfa11492d539f19
Daniel Kurniadi <daniel.thekurniadi@gmail.com>
2019-03-30 20:19:19 +0800

    fix and modify Double Triangle String Pattern

--- Strings/pattern-printing/double_triangle_xo.py
+++ Strings/pattern-printing/double_triangle_xo.py
@@ -1,28 +1,25 @@
 pattern = 'x o '
 def printing_awessome(n):
-    print_upper(n)
-    print_core(n)
-    print_lower(n)
+	print_upper(n)
+	print_core(n)
+	print_bottom(n)
 
 def print_upper(n):
-    for i in range(n-1):
-        print(' '*(n-1) + "".join([pattern[j%4] for j in range(i, -1, -1)]))
+	for i in range(n-1):
+		print(' '*(n-1) + "".join([pattern[j%4] for j in range(i, -1, -1)]))
 
 def print_core(n):
-    if n==1:
-        print('x')
-        return
-    print("".join([pattern[j%4] for j in range(n)]), end='')
-    print("".join([pattern[j%4] for j in range(n-2, -1, -1)]))
+	if n==1:
+		print('x')
+		return
+	print("".join([pattern[j%4] for j in range(n)]), end='')
+	print("".join([pattern[j%4] for j in range(n-2, -1, -1)]))
 
 def print_bottom(n):
-    for i in range(n-1, -1, -1):
-        print(' '*(n-i-1) + "".join([pattern[j%4] for j in range(i)]))
+	for i in range(n-1, 0, -1):
+		print(' '*(n-i) + "".join([pattern[j%4] for j in range(i)]))
 
 if __name__ == '__main__':
-    ns = [7, 2, 5, 6, 1, 8, 15]
-    for n in ns:
-        print("n = ", n)
-        print_upper(n)
-        print_core(n)
-        print_bottom(n)
+	n = int(input().strip())
+	printing_awessome(n)
+	
