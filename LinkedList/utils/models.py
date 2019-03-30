ff276cc53c783a01f833c08d7fb63599e12438c0
Daniel Kurniadi <daniel.thekurniadi@gmail.com>
2019-03-30 20:19:19 +0800

    remodel LinkedListHelper

--- LinkedList/utils/models.py
+++ LinkedList/utils/models.py
@@ -1,8 +1,10 @@
+from .helpers import LinkedListHelper
+
 class Node:
     def __init__(self, value):
         self.val = value
         self.next = None
 
-class LinkedList:
+class LinkedList(LinkedListHelper):
     def __init__(self, head=None):
         self.head = head
